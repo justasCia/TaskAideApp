dd679924e7511820a873b3844837b16aabd3bd5c
Justas Čiapas <justas.ciapas14@gmail.com>
2023-02-21 01:00:36 +0200

    login and register form 2 way binding

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Login {
+  email: string,
+  password: string
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  login: Login = {
+    email: '',
+    password: ''
+  };
+  submitted: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logInWithCredentials() {
+    this.submitted = true
+    console.log(this.login);
   }
 
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Register {
+  firstName: string,
+  lastname: string,
+  email: string,
+  phoneNumber: string,
+  password: string,
+  repeatPassword: string
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
+  register: Register = {
+    firstName: '',
+    lastname: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    repeatPassword: ''
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  registerWithCredentials() {
+    console.log(this.register);
   }
 
 }
