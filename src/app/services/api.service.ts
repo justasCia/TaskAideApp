eef5c624eb8042b0d222769c33cbd477308691fb
justas <justas.ciapas14@gmail.com>
2023-05-13 21:18:34 +0300

    add environments

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpOptions } from '@capacitor/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private domain: string = "https://localhost:7177/api/";
+  private domain: string = environment.apiUrl;
   constructor(private httpClient: HttpClient) { }
 
   private httpOptions = {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  production: true
+  apiUrl: 'https://app-taskaide-prod-noeu-001.azurewebsites.net/api/',
 };
